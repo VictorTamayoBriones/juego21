ca426aa4cc21a3d844a0202088f9c752777e7433
victor640 <54726734+victor640@users.noreply.github.com>
2020-09-08 00:05:37 -0500

    valor de cada carta
    se agrego la funcion para determinar el valor de cada carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -42,4 +42,18 @@ const pedirCarta = ()=>{
     return carta
 }
 
-pedirCarta();
+//pedirCarta();
+
+//esta funcion da el puntaje de cada carta
+
+valorCarta = (carta)=>{
+    
+    const valor = carta.substring(0, carta.length -1)
+    return (isNaN(valor))?
+            (valor=== 'A')? 11 : 10
+            :valor * 1;
+    
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({valor});
